f4c430764dabc6d486686a852625532a01a4d5ec
Elielso Francisco <107587443+Elielsouk@users.noreply.github.com>
2022-11-20 20:19:40 +0000

    Project5_comparisons_type_coercion
    Project5_comparisons_type_coercion.

---
+++ example.js
@@ -0,0 +1,8 @@
+function operator_Function() {
+  document.getElementById("Operator").innerHTML= !(5>10);//This is a Operator Function.
+}
+var a = 5 + 5; //Addition calculation.
+document.write(a + "<br>");//expression with a string.
+document.write(5>2&&10>4 + "<br>");//operator to show false.
+document.write("</br>");//string.
+document.write(5>10||10>4);//operator to show true.
